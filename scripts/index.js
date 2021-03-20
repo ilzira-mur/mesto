2534a38185edafe53b0c70b331a257a3e8e14e68
ilzira <murtazina.ilzi@yandex.ru>
2021-03-20 23:30:32 +0300

    form validation is added

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,5 @@
+const popup = document.querySelectorAll('.popup');
 const buttonsClosePopup = document.querySelectorAll('.popup__button_type_close');
-
 const buttonEdit = document.querySelector('.button_type_edit');
 const editFormModalWindow = document.querySelector('.popup_type_edit');
 const formElementEdit = document.querySelector('.popup__form_type_edit');
@@ -7,42 +7,45 @@ const nameInput = document.querySelector('.popup__input_type_name');
 const jobInput = document.querySelector('.popup__input_type_about');
 const nameInfo = document.querySelector('.info__name');
 const jobInfo = document.querySelector('.info__about');
-
 const buttonAddCard = document.querySelector('.button_type_add-card');
 const cardFormModalWindow = document.querySelector('.popup_type_add-card');
 const formElementCard = document.querySelector('.popup__form_type_new-card');
 const addCardNameInput = document.querySelector('.popup__input_type_card-name');
 const addCardLinkInput = document.querySelector('.popup__input_type_card-link');
-
 const zoomedPictureModalWindow = document.querySelector('.popup_type_zoomed');
 const popupZoomedPicture = document.querySelector('.popup__zoomed-picture');
 const popupZoomedName = document.querySelector('.popup__zoomed-name');
-
 const cardsList = document.querySelector('.cards');
 const templateElement = document.querySelector('.template');
 
+// открытие модального окна
 function openPopup(popup) {
   popup.classList.add('popup_type_opened');
 };
 
+// закрытие модального окна
 function closePopup(popup) {
   popup.classList.remove('popup_type_opened');
 };
 
+// открытие модального окна радактирования профиля
 function openPopupEdit() {
   savePopupEdit();
   openPopup(editFormModalWindow);
 };
 
+// копирование информации профиля в строки ввода модального окна
 function savePopupEdit() {
   nameInput.value = nameInfo.textContent;
   jobInput.value = jobInfo.textContent;
 };
-  
+
+// открытие модального окна добавления новой карточки
 function openPopupAddCard() {
   openPopup(cardFormModalWindow);
 };
 
+// открытие модального окна увеличенной картинки
 function handleZoomedPicture(event) {
   popupZoomedPicture.src = event.target.src;
   popupZoomedPicture.alt = event.target.alt;
@@ -50,6 +53,7 @@ function handleZoomedPicture(event) {
   openPopup(zoomedPictureModalWindow);
 };
 
+// сохранение внесенных измений профиля 
 function submitEditProfile(event) {
   event.preventDefault();
   nameInfo.textContent = nameInput.value;
@@ -57,6 +61,8 @@ function submitEditProfile(event) {
   closePopup(editFormModalWindow);
 };
 
+
+// форма добавления новой карточки
 function submitNewCard(event) {
   event.preventDefault();
   renderCard({
@@ -67,14 +73,19 @@ function submitNewCard(event) {
   closePopup(cardFormModalWindow);
 };
 
+
+// лайк карточки
 function handleLikeCard(event) {
   event.target.classList.toggle('card__button-like_active');
 };
 
+// удаление карточки
 function handleDeleteCard(event) {
   event.target.closest('.card').remove();
 };
 
+
+// создание новой карточки
 function createCard(name,  link) {
   const newItem = templateElement.content.cloneNode(true);
   const cardName = newItem.querySelector('.card__name');
@@ -90,22 +101,52 @@ function createCard(name,  link) {
   return newItem;
 };
 
+
+
+// рендер карточек
 function renderCard(card, cardsList) {
   cardsList.prepend(createCard(card.name, card.link));
  };
 
+ // прогрузка карточек на страницу
 initialCards.forEach((card) => {
   renderCard(card, cardsList)
  });
 
-buttonEdit.addEventListener('click', openPopupEdit);
-buttonAddCard.addEventListener('click', openPopupAddCard);
 
+
+// закрытие попапа кликом на оверлей 
+function mouseHandler(evt) {
+  if (evt.target.classList.contains('popup')){
+  closePopup(evt.target.closest('.popup'));
+};
+};
+
+// закрытие попапа нажатием на Esc
+function keyHandler(evt) {
+  if (evt.key === 'Escape') {
+    closePopup(cardFormModalWindow);
+    closePopup(editFormModalWindow);
+    closePopup(zoomedPictureModalWindow);
+  };
+};
+
+
+
+// слушатели
 buttonsClosePopup.forEach(popup => {
   popup.addEventListener('click', (evt) => {
     closePopup(evt.target.closest('.popup'));
   });
 });
 
+buttonEdit.addEventListener('click', openPopupEdit);
+buttonAddCard.addEventListener('click', openPopupAddCard);
 formElementEdit.addEventListener('submit', submitEditProfile);
 formElementCard.addEventListener('submit', submitNewCard);
+
+popup.forEach(popup => {
+  popup.addEventListener('mousedown', mouseHandler);
+});
+
+document.addEventListener('keydown', keyHandler);
---
+++ scripts/validate.js
@@ -0,0 +1,83 @@
+const obj = {
+    formSelectop: '.popup__form',
+    inputSelector: '.popup__input',
+    submitButtonSelector: '.popup__button_type_save',
+    errorSpanClass: 'span__error_active',
+    errorInputClass: 'popup__input_type_error',
+    inactiveButtonClass: 'popup__button_disabled',
+  }
+  
+  // запуск валидации
+  const enableValidation = ({formSelectop, inputSelector, submitButtonSelector, errorSpanClass, errorInputClass, inactiveButtonClass}) => {
+    const formList = Array.from(document.querySelectorAll(formSelectop));
+    
+    formList.forEach(
+      formElement => {formElement.addEventListener('submit', (event) => {
+          event.preventDefault();
+        });
+        setInputListeners(formElement, {inputSelector, submitButtonSelector, errorSpanClass, errorInputClass, inactiveButtonClass});
+      }
+    );
+  };
+  
+  // обработчик полей формы
+  const setInputListeners = (formElement, {inputSelector, submitButtonSelector, errorSpanClass, errorInputClass, inactiveButtonClass}) => {
+    const inputList = Array.from(formElement.querySelectorAll(inputSelector));
+    const buttonElement = formElement.querySelector(submitButtonSelector);
+    
+    inputList.forEach(
+      inputElement => {inputElement.addEventListener('input', () => {
+          checkInput(formElement, inputElement, {errorSpanClass, errorInputClass});
+          toggleButtonState(inputList, buttonElement, {inactiveButtonClass});
+        });
+      }
+    );
+  };
+  
+  // проверка валидности введеных данных
+  const checkInput = (formElement, inputElement, {errorSpanClass, errorInputClass}) => {
+    if (inputElement.validity.valid) {
+      hideInputError(formElement, inputElement, {errorSpanClass, errorInputClass});
+    } else {
+      showInputError(formElement, inputElement, {errorSpanClass, errorInputClass});
+    }
+  };
+  
+  // скрыть ошибку ввода и убрать подчеркивание поля ввода
+  const hideInputError = (formElement, inputElement, {errorSpanClass, errorInputClass}) => {
+    const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
+    errorElement.textContent = '';
+    errorElement.classList.remove(errorSpanClass);
+    inputElement.classList.remove(errorInputClass);
+  };
+  
+  // показать ошибку ввода и подчеркнуть поле ввода
+  const showInputError = (formElement, inputElement, {errorSpanClass, errorInputClass}) => {
+    const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
+    errorElement.textContent = inputElement.validationMessage;
+    errorElement.classList.add(errorSpanClass);
+    inputElement.classList.add(errorInputClass);
+  };
+  
+  // переключатель состояния кнопки
+  const toggleButtonState = (inputList, buttonElement, {inactiveButtonClass}) => {
+    if (hasInvalidInput(inputList) || hasEmptyInput(inputList)) {
+      buttonElement.classList.add(inactiveButtonClass);
+      buttonElement.setAttribute('disabled', true);
+    } else {
+      buttonElement.classList.remove(inactiveButtonClass);
+      buttonElement.removeAttribute('disabled');
+    }
+  };
+  
+  // проверка есть ли одно не валидное поле
+  const hasInvalidInput = (inputList) => {
+    return inputList.some(inputElement => !inputElement.validity.valid);
+  };
+  
+  // проверка есть ли пустое поле
+  const hasEmptyInput = (inputList) => {
+    return !inputList.some(inputElement => inputElement.value.length > 0);
+  };
+  
+  enableValidation(obj);
